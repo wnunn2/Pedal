91733ad318af4344a3158198f2d3b4b658f71564
richard lynch <richlynch2@gmail.com>
2020-02-26 13:33:31 +0900

    generated controller and changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :bicycles, only: %i[index show new create] do
-    resources :bookings, only: %i[show create]
+    resources :bookings, only: %i[create]
   end
+  resources :bookings, only: %i[show]
 end
