4913e49c7876801e0912babb37c8d293be756bcd
William Nunn <wnunnvfx@gmail.com>
2020-02-27 17:31:07 +0900

    Added booking listing cards and tabs to the booking index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :bicycles
   has_many :bookings
-  has_many :bookings_as_owner, through: :bicycles, source: :bookings
+  has_many :booking_requests, through: :bicycles, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :first_name, presence: true
@@ -9,4 +9,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
 end
