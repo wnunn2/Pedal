0558b1fb3f2b81ec361497392a9a6069b0d5bd18
William Nunn <wnunnvfx@gmail.com>
2020-02-27 14:01:28 +0900

    Update bicycles_controller.rb

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -15,23 +15,23 @@ class BicyclesController < ApplicationController
     @booking = Booking.new
   end
 
-  def new
-    @bicycle = Bicycle.new
-  end
-  def create
-    @bicycle = Bicycle.new
-    if @bicycle.save
-      redirect_to bicycle_path(@bicycle)
-    else
-      render :new
-    end
-  end
-  private
-  def bicycle_params
-    params.require(:bicycle).permit(:name, :photos [], :day_price, :address, :description, :category)
-  end
-  def find_bicycle
-    @bicycle = Bicycle.find(params[:id])
-  end
+#   def new
+#     @bicycle = Bicycle.new
+#   end
+#   def create
+#     @bicycle = Bicycle.new
+#     if @bicycle.save
+#       redirect_to bicycle_path(@bicycle)
+#     else
+#       render :new
+#     end
+#   end
+#   private
+#   def bicycle_params
+#     params.require(:bicycle).permit(:name, :photos [], :day_price, :address, :description, :category)
+#   end
+#   def find_bicycle
+#     @bicycle = Bicycle.find(params[:id])
+#   end
 
 end
