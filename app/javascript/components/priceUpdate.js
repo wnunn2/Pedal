75276e73ec3c2e0a6c87d37b2e3bddcc726d73f0
Natalia Takahara <nana_takahara@hotmail.com>
2020-02-28 12:32:54 +0900

    fix update price

--- app/javascript/components/priceUpdate.js
+++ app/javascript/components/priceUpdate.js
@@ -1,17 +1,27 @@
-const priceUpdate = () => {
+const changePriceValue = (event) => {
   const endDateInput = document.querySelector("#booking_end_date");
   const startDateInput = document.querySelector("#booking_start_date");
   const dayPrice = parseInt(document.querySelector('[data-price]').dataset.price, 10)
-  console.log(dayPrice)
-  endDateInput.addEventListener("change", (event) => {
-    const endDate = new Date(endDateInput.value)
-    const startDate = new Date(startDateInput.value)
-    // console.log(endDate)
-    const diffTime = Math.abs(endDate - startDate);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    const newPrice = diffDays * dayPrice;
-    const totalPrice = document.querySelector("#total-price");
+  const endDate = new Date(endDateInput.value)
+  console.log(typeof endDateInput.value)
+  const startDate = new Date(startDateInput.value)
+  const diffTime = Math.abs(endDate - startDate);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const newPrice = diffDays * dayPrice;
+  const totalPrice = document.querySelector("#total-price");
+  if (endDateInput.value !== "") {
     totalPrice.innerText = newPrice
-  })
+  }
+}
+
+const priceUpdate = () => {
+  const endDateInput = document.querySelector("#booking_end_date");
+  const startDateInput = document.querySelector("#booking_start_date");
+  if (endDateInput) {
+    endDateInput.addEventListener("change", changePriceValue)
+  }
+  if (startDateInput) {
+    startDateInput.addEventListener("change", changePriceValue)
+  }
 }
 export {priceUpdate}
